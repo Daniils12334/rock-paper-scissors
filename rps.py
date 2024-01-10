29658dc7e27ac7d087d7b3daa10c48195c476dd4
Daniils <you@example.com>
2024-01-10 10:18:53 +0200

    pappap

--- rps.py
+++ rps.py
@@ -5,10 +5,29 @@
 # 2. Dators iedomājas ciparu no 1 līdz 3  (izmanto random, https://www.w3schools.com/python/ref_random_randint.asp)
 # 3. Atkarībā no ievadītajiem cipariem un datora, izdrukā paziņojumu par zaudējumu, uzvaru, vai neizšķirtu (izmanto if, else https://www.w3schools.com/python/python_conditions.asp)
 #
-
+import random
 ROCK = "1"
 SCISSOR = "2"
 PAPER = "3"
 
 print("Welcome to Rock, Paper, Scissors!")
+print("Enter your hand")
+print("1 - rock")
+print("3 - paper")
+print("2 - scissors")
+player_choice = input()
 
+computer_choice = str(random.randint(1,3))
+
+if player_choice == computer_choice:
+    print("tie")
+elif (
+    (player_choice == ROCK and computer_choice == SCISSOR)
+    or (player_choice == SCISSOR and computer_choice == PAPER)
+    or(player_choice == PAPER and computer_choice == ROCK)
+    ):
+    print("computer choice = "+computer_choice)
+    print("You win")
+else:
+    print("computer choice = "+computer_choice)
+    print("You lose")
--- rps5.py
+++ rps5.py
@@ -5,3 +5,49 @@
 # 5. [Bonus Level] Izmantot ne vairāk par trīs if/else rezultāta skaitīšanai (var izmantot ciparu starpību)
 # 6. [UNSTOPPABLE] Pievieno papildus ieročus Rock-Paper-Scissors-Spock-Lizard (https://en.wikipedia.org/wiki/Rock_paper_scissors#Additional_weapons)
 # 
+import random
+ROCK = "1"
+SCISSOR = "2"
+PAPER = "3"
+SPOCK = "4"
+LIZARD = "5"
+x = 0
+y = 0
+z = 0
+
+for _ in range(5):
+    print("Welcome to Rock, Paper, Scissors! 5 Rounds version")
+    print("Enter your hand")
+    print("1 - rock")
+    print("3 - paper")
+    print("2 - scissors")
+    print("4 - spock")
+    print("5 - lizard")
+    player_choice = input()
+    computer_choice = str(random.randint(1,3))
+    if player_choice == computer_choice:
+        print("tie")
+        z = z+1
+    elif (
+        (player_choice == ROCK and computer_choice == SCISSOR) or (player_choice == ROCK and computer_choice == LIZARD)
+        or (player_choice == SCISSOR and computer_choice == PAPER)or (player_choice == SCISSOR and computer_choice == LIZARD)
+        or(player_choice == PAPER and computer_choice == ROCK) or (player_choice == PAPER and computer_choice == SPOCK)
+        or(player_choice == SPOCK and computer_choice == SCISSOR) or (player_choice == SPOCK and computer_choice == ROCK)
+        or(player_choice == LIZARD and computer_choice == SPOCK)or(player_choice == LIZARD and computer_choice == PAPER)
+        ):
+        print("computer choice = "+computer_choice)
+        print("You win")
+        x = x+1
+    else:
+        print("computer choice = "+computer_choice)
+        print("You lose")
+        y=y+1
+        print("Next round")
+print("your wins")
+print(x)
+print("your loses")
+print(y)
+print("your ties")
+print(z)
+
+
